1af7e4be00cc9511bf8deb27e180418ff81345ea
Nicolas Anguiano <nbanguiano@users.noreply.github.com>
2014-05-26 15:03:06 +0200

    Update info_popup.js

--- info_popup.js
+++ info_popup.js
@@ -3,7 +3,7 @@ javascript:(function(){
 	var o = document.createElement('div'); 
 	o.id = 'signature'; 
 	o.style.position = 'absolute'; 
-	o.style.zIndex = '999';
+	o.style.zIndex = '999999';
 	o.style.backgroundColor = 'rgba(0,0,0,0.7)'; 
 	o.style.width = window.innerWidth + 'px'; 
 	o.style.height = window.innerHeight + 'px'; 
@@ -16,7 +16,7 @@ javascript:(function(){
 	t.style.display = 'block';
 	t.style.position = 'absolute'; 
 	t.style.marginLeft = '25%';
-	t.style.marginTop = '5%'; 
+	t.style.marginTop = '10%'; 
 	
 	var text = 'WHATEVER_YOU_WANT_TO_DISPLAY'
 	t.value = text;
@@ -24,7 +24,6 @@ javascript:(function(){
 	var h3 = document.createElement('H3');
 	h3.style.marginLeft = '25%';
 	h3.style.marginTop = '100px';
-	h3.style.zIndex = '1000';
 	h3.style.color = 'white';
 	h3.style.cursor = 'pointer';
 	var c = document.createTextNode('Close X');
